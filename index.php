a20f6894adad6d5b61e174e7e212a644dabca6a6
mEsUsah <stanley@haxor.no>
2025-09-24 21:09:49 +0100

    fix: test index file

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Directory Listing</h1>
+    <ul>
+        <?php
+            // list all directories and files
+            $files = scandir(__DIR__);
+            foreach ($files as $file) {
+                echo '<li><a href="' . $file . '">' . $file . '</a></li>';
+            }
+        ?>
+    </ul>
+</body>
+</html>
