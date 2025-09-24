892d0b26101f99afa27b7d59402c6a7ea310a2de
mEsUsah <stanley@haxor.no>
2025-09-24 21:58:06 +0100

    fix: add week 1 index

---
+++ week1/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>USW - AIM</title>
+</head>
+<body>
+    <h1>Advanced Internet and Mobile Computing</h1>
+    <p>Work during my USW studies autumn 2025</p>
+    <p>Week 1 (2025.09.22 - 2025.09.28)</p>
+    <ul>
+        <?php
+            $files = scandir(__DIR__);
+            foreach ($files as $file) {
+                // only list visible files, not directories and only files with the htm extension
+                if (!is_dir($file) && substr($file, 0, 1) !== '.' && pathinfo($file, PATHINFO_EXTENSION) === 'htm') {
+                    echo '<li><a href="' . $file . '">' . $file . '</a></li>';
+                }
+            }
+        ?>
+    </ul>
+</body>
+</html>
