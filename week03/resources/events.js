1edc6c93429e3dd2aa61057b6d14260804c46455
mEsUsah <stanley@haxor.no>
2025-10-08 11:24:22 +0100

    fix(week3): add event listeners to all list items

--- week03/resources/events.js
+++ week03/resources/events.js
@@ -1,16 +1,22 @@
-function doEvents(){
-  var el = document.getElementById('two');
-
-  el.addEventListener('click', toggleStatus, false);
+function doEvents() {
+    var list = document.getElementById('course-list')?.children;
+    if (list && list.length > 0) {
+        // cast HTMLCollection to Array
+        Array.prototype.forEach.call(list, (item) => {
+            item.addEventListener('click', toggleStatus, false);
+        });
+    }
 }
 
-function toggleStatus(){
-  if (this.className === 'full'){
-    this.className = 'running';}
-  else if (this.className === 'running'){
-    this.className = 'full';
-  }
+function toggleStatus(event) {
+    var element = event.target;
+    console.log(element);
+    if (element.className === 'full') {
+        element.className = 'running';
+    }
+    else if (element.className === 'running') {
+        element.className = 'full';
+    }
 }
 
-console.log("Script loaded");
 doEvents();
