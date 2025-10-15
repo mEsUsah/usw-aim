4902a847cf7698657ec85d56af9832778801f257
mEsUsah <stanley@haxor.no>
2025-10-15 16:30:32 +0100

    fix(canvas-GameShapeAnimation): add suppor for delete when done

--- canvas/resources/components/GameObject.js
+++ canvas/resources/components/GameObject.js
@@ -4,23 +4,28 @@ export class GameObject {
         this.y = y;
         this.animations = [];
     }
-    addAnimation(length, loop, startDelay) {
-        const animation = new GameObjectAnimation(length, loop, startDelay);
+
+    addAnimation(length, loop, startDelay, deleteTime) {
+        const animation = new GameShapeAnimation(length, loop, startDelay, deleteTime);
         this.animations.push(animation);
+    }
+
+    deleteCompletedAnimations() {
+        this.animations = this.animations.filter(animation => !animation.getIsPastDeleteTime());
     }
 
 }
 
-class GameObjectAnimation {
+class GameShapeAnimation {
     static INFINITE = -1;
-    constructor(duration, loop, startDelay) {
+    constructor(duration, loop, startDelay, deleteTime) {
         this.duration = duration;
         this.progress = 0;
         this.lifetime = 0;
         this.loop = loop; // -1 inifinitely, 0 no loop, n times
         this.loopCount = 1;
         this.startDelay = startDelay || 0;
-
+        this.deleteTime = deleteTime || 0;
     }
 
     updateProgress(deltaTime) {
@@ -35,7 +40,7 @@ class GameObjectAnimation {
 
         // Looping logic
         if(this.progress > this.duration 
-            && (this.loop === GameObjectAnimation.INFINITE || this.loopCount < this.loop)
+            && (this.loop === GameShapeAnimation.INFINITE || this.loopCount < this.loop)
         ) {
             this.loopCount++;
             this.progress = this.progress % this.duration;
@@ -51,5 +56,14 @@ class GameObjectAnimation {
 
     getProgress() {
         return this.progress / this.duration;
+    }
+
+    getIsPastDeleteTime() {
+        if(this.deleteTime > 0 
+            && this.lifetime > (this.startDelay + this.deleteTime)
+        ) {
+            return true;
+        }
+        return false;
     }
 }
