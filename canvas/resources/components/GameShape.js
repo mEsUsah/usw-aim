17c5d63174de6b681593fb35af6ae2fcb37da17d
mEsUsah <stanley@haxor.no>
2025-10-22 13:32:27 +0100

    fix(canvas-GameShape): clean up draw method

--- canvas/resources/components/GameShape.js
+++ canvas/resources/components/GameShape.js
@@ -33,23 +33,28 @@ export default class GameShape {
             direction = this.animation.config.direction;
         }
 
-        if(this.type === 'circle') {
-            ctx.beginPath();
-            if(direction === GameShapeAnimation.FORWARD) {
-                ctx.arc(0, 0, this.config.radius, 0, (2 * Math.PI * progress), false);
-            } else {
-                if(progress === 1) progress = 0; // To avoid full circle when finished
-                ctx.arc(0, 0, this.config.radius, (2 * Math.PI * progress), 0, false);
-            }
-            ctx.stroke();
-        }
+        switch(this.type) {
+            case 'circle':
+                ctx.beginPath();
+                if(direction === GameShapeAnimation.FORWARD) {
+                    ctx.arc(0, 0, this.config.radius, 0, (2 * Math.PI * progress), false);
+                } else {
+                    if(progress === 1) progress = 0; // To avoid full circle when finished
+                    ctx.arc(0, 0, this.config.radius, (2 * Math.PI * progress), 0, false);
+                }
+                ctx.stroke();
+                break;
+            
+            case 'line':
+                ctx.beginPath();
+                ctx.moveTo(0, 0);
+                ctx.lineTo((this.config.x2 - this.config.x) * progress, (this.config.y2 - this.config.y) * progress);
+                ctx.stroke();
+                break;
 
-
-        if(this.type == 'line') {
-            ctx.beginPath();
-            ctx.moveTo(0, 0);
-            ctx.lineTo((this.config.x2 - this.config.x) * progress, (this.config.y2 - this.config.y) * progress);
-            ctx.stroke();
+            default:
+                console.warn(`GameShape::draw - Unknown shape type: ${this.type}`);
+                break;
         }
 
         ctx.restore();
