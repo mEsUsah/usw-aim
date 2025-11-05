67a9e8627f42ee999e0e6707650fa28bfdca9c6c
mEsUsah <stanley@haxor.no>
2025-11-05 13:57:47 +0000

    fix(checkWinCondition): adapt to check for all directions

--- resources/components/checkWinCondition.js
+++ resources/components/checkWinCondition.js
@@ -1,32 +1,61 @@
 export default function(game, selectedTile){
     console.log("clicked: x:" + selectedTile.config.location.x + ", y:" + selectedTile.config.location.y);
-    // Check west-east win
-    // find first to the west
+    
+    console.log("checking →");
+    if(checkWin(game, selectedTile, 1, 0)){
+        console.log("Win!!");
+        return
+    }
+
+    console.log("checking ↘");
+    if(checkWin(game, selectedTile, 1, 1)){
+        console.log("Win!!");
+        return
+    }
+
+    console.log("checking ↓");
+    if(checkWin(game, selectedTile, 0, 1)){
+        console.log("Win!!");
+        return
+    }
+    
+    console.log("checking ↙");
+    if(checkWin(game, selectedTile, -1, 1)){
+        console.log("Win!!");
+        return
+    }
+}
+
+function checkWin(game, selectedTile, xDirection, yDirection){
+    // find first (draw bow)
     let firstFoundLocation = selectedTile.config.location;
     let currentX = selectedTile.config.location.x;
     let currentY = selectedTile.config.location.y;
-    while(currentX >= 0){
-        console.log(game.gameFields[currentX][currentY]);
+    while(currentX >= 0 && currentY >= 0 && currentX < game.config.boardSize && currentY < game.config.boardSize){
+        // stop search if checked tile is unused or occupied by anything else than the current player
         if(!game.gameFields[currentX][currentY].state || game.gameFields[currentX][currentY].state.occupiedBy != selectedTile.state.occupiedBy){
             break;
         }
         firstFoundLocation = game.gameFields[currentX][currentY].config.location;
-        currentX--;
+        currentX -= xDirection;
+        currentY -= yDirection;
     }
-    // count from west to east
+
+    // count (shoot arrow)
     let score = 0;
     currentX = firstFoundLocation.x;
     currentY = firstFoundLocation.y;
-    while(currentX < game.config.boardSize){
+    while(currentX < game.config.boardSize && currentY < game.config.boardSize && currentX >= 0 && currentY >= 0){
+        // stop counting if chekced tile is unused or occupied by anyting else than the current player
         if(!game.gameFields[currentX][currentY].state || game.gameFields[currentX][currentY].state.occupiedBy != selectedTile.state.occupiedBy){
             break;
         }
         score++;
-        console.log("scored! current score: " + score);
         if (score >= game.state.winLength){
-            console.log("PLAYER " + selectedTile.state.occupiedBy + " WINS!");
-            return;
+            return true;
         }
-        currentX++;
+        currentX += xDirection;
+        currentY += yDirection;
     }
+    return false;
 }
