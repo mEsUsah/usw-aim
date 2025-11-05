465ef95ad47751a560aaeb8f124a30d426d0399d
mEsUsah <stanley@haxor.no>
2025-11-05 11:39:04 +0000

    fix(gameBoard): correct coordinates, and add location attributs to config

--- resources/components/gameBoard.js
+++ resources/components/gameBoard.js
@@ -7,20 +7,24 @@ export function create(game){
     game.gameFields = [];
 
     // Initialize board cells
-    for (let i = 0; i < game.config.boardSize; i++) {
+    for (let x = 0; x < game.config.boardSize; x++) {
         let gameFieldRow = [];
-        for (let j = 0; j < game.config.boardSize; j++) {
+        for (let y = 0; y < game.config.boardSize; y++) {
             const gameObject = new GameObject({
                 variant: GameObject.VARIANT.BOARD,
-                x: game.config.cellWidth / 2 + j * game.config.cellWidth + game.config.boardMargin,
-                y: game.config.cellHeight / 2 + i * game.config.cellHeight + game.config.boardMargin,
-                name: `board_${i}_${j}`,
+                x: game.config.cellWidth / 2 + x * game.config.cellWidth + game.config.boardMargin,
+                y: game.config.cellHeight / 2 + y * game.config.cellHeight + game.config.boardMargin,
+                name: `board_tile`,
                 outline: {
                     top: game.config.cellHeight / 2,
                     left: game.config.cellWidth / 2,
                     bottom: game.config.cellHeight / 2,
                     right: game.config.cellWidth / 2
-                }
+                },
+                location: {
+                    x: x,
+                    y: y,
+                },
             });
             const shape = new GameShape('rectangle', {
                 x: -game.config.cellWidth / 2,
