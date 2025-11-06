afdea62dcd9ee63e8c232fb74e5ef1bc5dcf0e1a
mEsUsah <stanley@haxor.no>
2025-11-06 17:55:12 +0000

    fix(cpuInput): document and clean up

--- resources/components/cpuInput/cpuInput.js
+++ resources/components/cpuInput/cpuInput.js
@@ -4,18 +4,28 @@ import checkDrawCondition from '../checkDrawCondition.js';
 import * as uiGameplay from '../ui/uiGameplay.js';
 import * as gameplayObjects from '../gameObjects/gameplayObjects.js';
 
+/** 
+ * Handles CPU inputs during gameplay.
+ * Emulates a player making a decision.
+ * @param {Game} game - The game instance.
+ */
 export function handleCpuInput(game){
-    // Wait to give illusion of decision time
+    // Wait a moment to give illusion of decision time
     game.state.cpuWaitTime -= game.frameData.deltaTime;
     if(game.state.cpuWaitTime > 0){
         return;
     }
     game.state.cpuWaitTime = game.state.cpuMoveDelay;
     
-    pickRandomTile(game);
+    pickRandomField(game);
 }
 
-function pickRandomTile(game){
+/**
+ * Pick a random unoccupied field on the game board.
+ * @param {Game} game  - The game instance.
+ * @returns 
+ */
+function pickRandomField(game){
     while(true){
         const randX = Math.floor(Math.random() * game.state.boardSize);
         const randY = Math.floor(Math.random() * game.state.boardSize);
